91ae2cd40b9c55c33e3144d4a43c38a1c5bfdd68
Mishael <mishaelclifford1@gmail.com>
2023-04-13 12:42:28 +0100

    Market place new changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,28 +14,28 @@ function Sidebar() {
           <div className="rounded-full h-52 w-52">
             <img src={Profile} className="object-contain object-cover" />
           </div>
-          <div className="flex items-center  space-x-3">
+          <div className="flex items-center cursor-pointer space-x-3">
             <GoDashboard  size={22} className="text-[#e6d646]"/>
             <h1 class="font-semibold text-base text-[#e6d646]">Dashboard</h1>
           </div>
-          <div className="flex items-center  space-x-3">
+          <div className="flex items-center  cursor-pointer space-x-3">
             <FiArchive  size={22} className="text-white"/>
-            <h1 class="font-semibold text-base text-white">Products</h1>
+            <h1 class="font-semibold text-base  text-white">Products</h1>
           </div>
-          <div className="flex items-center  space-x-3">
+          <div className="flex items-center   cursor-pointer space-x-3">
             <HiOutlineUserGroup  size={22} className="text-white"/>
             <h1 class="font-semibold text-base text-white">Customers</h1>
           </div>
-          <div className="flex items-center  space-x-3">
+          <div className="flex items-center   cursor-pointer space-x-3">
             <MdOutlineReviews  size={22} className="text-white"/>
             <h1 class="font-semibold text-base text-white">Reviews</h1>
           </div>
           <div className="space-y-[50%]">
-          <div className="flex items-center  space-x-3">
+          <div className="flex items-center   cursor-pointer space-x-3">
             <TbSettings  size={22} className="text-white"/>
             <h1 class="font-semibold text-base text-white">Settings</h1>
           </div>
-          <div className=" flex items-center space-x-3">
+          <div className=" flex items-center  cursor-pointer space-x-3">
           <BiLogOut size={22} className="text-white"/>
           <h1 class="font-semibold text-base text-white">Log out</h1>
         </div>
